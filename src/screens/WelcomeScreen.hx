6ab0cbac7bab36068307ac9145a4c9f145bc3534
SidGames5 <saytleader@outlook.com>
2024-02-11 09:06:07 -0500

    Get user input

--- src/screens/WelcomeScreen.hx
+++ src/screens/WelcomeScreen.hx
@@ -1,5 +1,11 @@
 package screens;
 
 class WelcomeScreen {
-	public static function start() {}
+	public static function start() {
+		while (true) {
+			Sys.println("Welcome to Spiel");
+			Sys.print("Would you like to [L]ogin or [S]ign up?: ");
+			var input = Sys.stdin().readLine().toLowerCase();
+		}
+	}
 }
