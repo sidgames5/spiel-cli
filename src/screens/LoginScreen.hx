e019f959cb3c0c5bd90ad4269bf4aa9c6033133a
SidGames5 <saytleader@outlook.com>
2024-02-11 09:14:15 -0500

    Add screens

---
+++ src/screens/LoginScreen.hx
@@ -0,0 +1,5 @@
+package screens;
+
+class LoginScreen {
+	public static function start() {}
+}
--- src/screens/WelcomeScreen.hx
+++ src/screens/WelcomeScreen.hx
@@ -11,7 +11,11 @@ class WelcomeScreen {
 	private static function prompt() {
 		Sys.print("Would you like to [L]ogin or [S]ign up?: ");
 		var input = Sys.stdin().readLine().toLowerCase();
-		if (input == "L") {} else if (input == "S") {} else {
+		if (input == "L") {
+			LoginScreen.start();
+		} else if (input == "S") {
+			SignUpScreen.start();
+		} else {
 			Sys.print("\033[A\r\033[2K");
 			Sys.println("Invalid input");
 			prompt();
